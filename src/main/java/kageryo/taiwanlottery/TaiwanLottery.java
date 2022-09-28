62e2ece358e97e8705642e5312a42a8cf86803d7
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-09-28 10:24:22 +0800

    Update TaiwanLottery.java

--- src/main/java/kageryo/taiwanlottery/TaiwanLottery.java
+++ src/main/java/kageryo/taiwanlottery/TaiwanLottery.java
@@ -8,8 +8,7 @@ public final class TaiwanLottery extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         taiwanLottery=this;
-        this.getLogger().info("TaiwanLottery Plugin By.K6Dev@DreamFuture-Studio");
-
+        this.getLogger().info("TaiwanLottery Plugin By.K6Dev@DreamFuture-Studio"); // 插件歡迎訊息(檢查是否運作用)
     }
 
     @Override
